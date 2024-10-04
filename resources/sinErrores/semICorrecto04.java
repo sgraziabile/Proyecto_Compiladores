b4020629cf8ebb0e8093e3d7004db2accac5ad1d
sgraziabile <sgraziabile@gmail.com>
2024-10-04 10:12:57 -0300

    Default constructor added if not declared.

---
+++ resources/sinErrores/semICorrecto04.java
@@ -0,0 +1,20 @@
+///[SinErrores]
+// Prueba atributos y herencia
+class A  {
+    static int x;
+    B y;
+}
+class B extends A {
+   A z;
+}
+
+
+
+class Init{
+    static void main()
+    { }
+}
+
+
+
+
